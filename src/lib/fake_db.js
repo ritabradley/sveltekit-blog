29482f58520c8b0e463136f350298ebd975b929b
Rita Bradley <rita@ritabradley.dev>
2023-06-21 15:44:05 -0700

    add fake database file

---
+++ src/lib/fake_db.js
@@ -0,0 +1,28 @@
+export const fakeData = [
+  {
+    id: 1,
+    author: 'Rita Bradley',
+    title: 'Learning SvelteKit with egghead.io',
+    slug: 'learning-sveltekit-with-egghead-io',
+    date: '2023-06-21',
+    content: 'Today I started learning SvelteKit with egghead.io. I am excited to learn this new framework.',
+    published: true,
+  },
+  {
+    id: 2,
+    author: 'Rita Bradley',
+    title: 'Trying To Study With Small Kids Around',
+    slug: 'trying-to-study-with-small-kids-around',
+    date: '2023-06-22',
+    content: 'Today I tried to study with my kids around. It was hard to focus.',
+    published: false,
+  },
+  {
+    id: 3,
+    author: 'Rita Bradley',
+    title: 'Learning To Code While Struggling With Depression',
+    slug: 'learning-to-code-while-struggling-with-depression',
+    date: '2023-06-23',
+    content: 'Depression is an insidious disease. It is hard to focus on learning to code when you are depressed.',
+  },
+];
