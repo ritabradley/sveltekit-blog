04390182b0eb6cd062d1fd22793b98690fcf1cdd
Rita Bradley <rita@ritabradley.dev>
2023-06-21 16:49:54 -0700

    rename to `page.server.js`

--- src/routes/posts/+page.js
+++
@@ -1,9 +0,0 @@
-import { fakeData } from '../../lib/fake_db';
-
-/** @type {import('./$types').PageLoad} */
-
-export function load() {
-  return {
-    posts: fakeData,
-  };
-}
---
+++ src/routes/posts/+page.server.js
@@ -0,0 +1,10 @@
+import { getAllPosts } from '../../lib/sanity';
+
+/** @type {import('./$types').PageServerLoad} */
+
+export async function load() {
+  const posts = await getAllPosts();
+  return {
+    posts,
+  };
+}
