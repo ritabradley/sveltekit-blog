4df593827b04cdebf5ede37d76eb4cf1e77e4ab7
Rita Bradley <rita@ritabradley.dev>
2023-06-21 15:44:33 -0700

    crreate dynamic route for individual posts

---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  /** @type {import('./$types').PageData} */
+  export let data;
+</script>
+
+<h1 class="text-4xl font-bold">{data.post.title}</h1>
+<p class="text-2xl">{data.post.author}</p>
+<small class="text-sm text-slate-400">{data.post.date}</small>
+<p class="text-lg">{data.post.content}</p>
