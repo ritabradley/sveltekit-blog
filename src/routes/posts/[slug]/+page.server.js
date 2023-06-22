172e19cbe9a5123fe2c9f8d97e25eafd7a6d0d8e
Rita Bradley <rita@ritabradley.dev>
2023-06-21 17:56:46 -0700

    load single post dynamically

---
+++ src/routes/posts/[slug]/+page.server.js
@@ -0,0 +1,15 @@
+import { getPostBySlug } from '../../../lib/sanity';
+import { compile } from 'mdsvex';
+
+/** @type {import('./$types').PageServerLoad} */
+
+export async function load({ params }) {
+  const routeSlug = params.slug;
+  const getSinglePost = await getPostBySlug(routeSlug);
+  const { title, slug, content, ...rest } = getSinglePost[0];
+  const post = { title, slug, content: compile(content), ...rest };
+
+  return {
+    post,
+  };
+}
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -4,6 +4,13 @@
 </script>
 
 <h1 class="text-4xl font-bold">{data.post.title}</h1>
-<p class="text-2xl">{data.post.author}</p>
-<small class="text-sm text-slate-400">{data.post.date}</small>
-<p class="text-lg">{data.post.content}</p>
+
+<p class="text-2xl">{data.post.author.name}</p>
+<small class="text-slate-400 text-sm">{data.post.date}</small>
+<p class="text-lg">
+  {#await data.post.content}
+    <p>...loading</p>
+  {:then content}
+    {@html content?.code}
+  {/await}
+</p>
